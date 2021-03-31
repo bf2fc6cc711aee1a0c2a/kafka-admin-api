06afc8f88a25dfe4341359b9e1b237256aeb2cbc
Stanislav Knot <sknot@redhat.com>
2021-03-31 10:23:26 +0200

    Update dependencies (#22)
    * Update dependencies
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * fixes
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * rebase + fix
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * fix tests
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * more fixes
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * fix pom
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- rest/src/main/java/org/bf2/admin/rest/RestService.java
+++ rest/src/main/java/org/bf2/admin/rest/RestService.java
@@ -10,7 +10,7 @@ import org.bf2.admin.kafka.admin.handlers.RestOperations;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
+import io.vertx.ext.web.openapi.RouterBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,16 +29,17 @@ public class RestService implements RouteRegistration {
 
         final Promise<RouteRegistrationDescriptor> promise = Promise.promise();
 
-        OpenAPI3RouterFactory.create(vertx, "openapi-specs/rest.yaml", ar -> {
+        RouterBuilder.create(vertx, "openapi-specs/rest.yaml", ar -> {
             if (ar.succeeded()) {
                 try {
                     kaConfig = new KafkaAdminConfigRetriever();
                 } catch (Exception e) {
                     promise.fail(e);
+                    return;
                 }
-                OpenAPI3RouterFactory routerFactory = ar.result();
-                assignRoutes(routerFactory, vertx);
-                promise.complete(RouteRegistrationDescriptor.create("/rest", routerFactory.getRouter()));
+                RouterBuilder routerBuilder = ar.result();
+                assignRoutes(routerBuilder, vertx);
+                promise.complete(RouteRegistrationDescriptor.create("/rest", routerBuilder.createRouter()));
                 log.info("Rest server version {} started.", RestService.class.getPackage().getImplementationVersion());
             } else {
                 promise.fail(ar.cause());
@@ -48,35 +49,23 @@ public class RestService implements RouteRegistration {
         return promise.future();
     }
 
-    private void assignRoutes(final OpenAPI3RouterFactory routerFactory, final Vertx vertx) {
+    private void assignRoutes(final RouterBuilder routerFactory, final Vertx vertx) {
         RestOperations ro = new RestOperations();
-        routerFactory.addHandlerByOperationId(Operations.GET_TOPIC, ro.describeTopic(kaConfig, vertx, httpMetrics));
-        routerFactory.addHandlerByOperationId(Operations.GET_TOPICS_LIST, ro.listTopics(kaConfig, vertx, httpMetrics));
+        routerFactory.operation(Operations.GET_TOPIC).handler(ro.describeTopic(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
+        routerFactory.operation(Operations.GET_TOPICS_LIST).handler(ro.listTopics(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
 
-        routerFactory.addHandlerByOperationId(Operations.DELETE_TOPIC, ro.deleteTopic(kaConfig, vertx, httpMetrics));
-        routerFactory.addHandlerByOperationId(Operations.CREATE_TOPIC, ro.createTopic(kaConfig, vertx, httpMetrics));
-        routerFactory.addHandlerByOperationId(Operations.UPDATE_TOPIC, ro.updateTopic(kaConfig, vertx, httpMetrics));
+        routerFactory.operation(Operations.DELETE_TOPIC).handler(ro.deleteTopic(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
+        routerFactory.operation(Operations.CREATE_TOPIC).handler(ro.createTopic(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
+        routerFactory.operation(Operations.UPDATE_TOPIC).handler(ro.updateTopic(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
 
-        routerFactory.addHandlerByOperationId(Operations.GET_CONSUMER_GROUP, ro.describeGroup(kaConfig, vertx, httpMetrics));
-        routerFactory.addHandlerByOperationId(Operations.GET_CONSUMER_GROUPS_LIST, ro.listGroups(kaConfig, vertx, httpMetrics));
+        routerFactory.operation(Operations.GET_CONSUMER_GROUP).handler(ro.describeGroup(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
+        routerFactory.operation(Operations.GET_CONSUMER_GROUPS_LIST).handler(ro.listGroups(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
 
-        routerFactory.addHandlerByOperationId(Operations.DELETE_CONSUMER_GROUP, ro.deleteGroup(kaConfig, vertx, httpMetrics));
+        routerFactory.operation(Operations.DELETE_CONSUMER_GROUP).handler(ro.deleteGroup(kaConfig, vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
 
-        routerFactory.addHandlerByOperationId(Operations.OPEN_API, ro.openApi(vertx, httpMetrics));
+        routerFactory.operation(Operations.OPEN_API).handler(ro.openApi(vertx, httpMetrics)).failureHandler(ro.errorHandler(httpMetrics));
 
-        routerFactory.addHandlerByOperationId(Operations.METRICS, routingContext -> routingContext.response().setStatusCode(HttpResponseStatus.OK.code()).end(httpMetrics.getRegistry().scrape()));
+        routerFactory.operation(Operations.METRICS).handler(routingContext -> routingContext.response().setStatusCode(HttpResponseStatus.OK.code()).end(httpMetrics.getRegistry().scrape()));
 
-
-        routerFactory.addFailureHandlerByOperationId(Operations.GET_TOPIC, ro.errorHandler(httpMetrics));
-        routerFactory.addFailureHandlerByOperationId(Operations.GET_TOPICS_LIST, ro.errorHandler(httpMetrics));
-
-        routerFactory.addFailureHandlerByOperationId(Operations.DELETE_TOPIC, ro.errorHandler(httpMetrics));
-        routerFactory.addFailureHandlerByOperationId(Operations.CREATE_TOPIC, ro.errorHandler(httpMetrics));
-        routerFactory.addFailureHandlerByOperationId(Operations.UPDATE_TOPIC, ro.errorHandler(httpMetrics));
-
-        routerFactory.addFailureHandlerByOperationId(Operations.GET_CONSUMER_GROUP, ro.errorHandler(httpMetrics));
-        routerFactory.addFailureHandlerByOperationId(Operations.GET_CONSUMER_GROUPS_LIST, ro.errorHandler(httpMetrics));
-
-        routerFactory.addFailureHandlerByOperationId(Operations.DELETE_CONSUMER_GROUP, ro.errorHandler(httpMetrics));
     }
 }
