352145e9a615fef87cb56a06de3cae3b6e1ff9a6
Gerard Ryan <gerard@ryan.lt>
2021-04-09 11:17:16 +0100

    Calculate consumer group lag
    Before this change, calls to list or describe consumer groups would
    include fields for lag and logEndOffset in their results (per
    partition), but the values of these fields would always be 0.
    
    This change figures out all of the TopicPartitions for which lag needs
    to be calculated, and fetches the latest offsets for these
    partitions. Then the lag is calculated as the difference between the
    consumer group offset on a partition, and the latest offset of the
    partition.

--- kafka-admin/src/main/java/org/bf2/admin/kafka/admin/ConsumerGroupOperations.java
+++ kafka-admin/src/main/java/org/bf2/admin/kafka/admin/ConsumerGroupOperations.java
@@ -8,6 +8,8 @@ import io.vertx.core.Promise;
 import io.vertx.kafka.admin.ConsumerGroupDescription;
 import io.vertx.kafka.admin.ConsumerGroupListing;
 import io.vertx.kafka.admin.KafkaAdminClient;
+import io.vertx.kafka.admin.ListOffsetsResultInfo;
+import io.vertx.kafka.admin.OffsetSpec;
 import io.vertx.kafka.client.common.TopicPartition;
 import io.vertx.kafka.client.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.errors.GroupIdNotFoundException;
@@ -18,8 +20,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.BinaryOperator;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -55,20 +60,42 @@ public class ConsumerGroupOperations {
                 return CompositeFuture.join(promises);
             })
             .compose(descriptions -> {
-                List<Types.ConsumerGroupDescription> list = new ArrayList<>();
+                Map<TopicPartition, OffsetSpec> topicPartitionOffsetSpecs = new HashMap<>();
+                Map<String, ConsumerGroupDescription> cgDescriptions = new HashMap<>();
+                Map<String, Map<TopicPartition, OffsetAndMetadata>> cgTopicPartitionOffsets = new HashMap<>();
                 for (int i = 0; i < descriptions.result().size(); i += 2) {
-                    Map<String, ConsumerGroupDescription> desc = descriptions.resultAt(i);
-                    Map<TopicPartition, OffsetAndMetadata> off = descriptions.resultAt(i + 1);
-                    Types.ConsumerGroupDescription item = getConsumerGroupsDescription(pattern, desc, off).get(0);
-                    if (item != null) {
-                        list.add(item);
-                    }
+                    Map<String, ConsumerGroupDescription> cgDescription = descriptions.resultAt(i);
+                    List<TopicPartition> topicPartitions = getTopicPartitions(cgDescription);
+                    topicPartitions.forEach(topicPartition -> topicPartitionOffsetSpecs.putIfAbsent(topicPartition, OffsetSpec.LATEST));
+
+                    cgDescriptions.putAll(cgDescription);
+                    cgTopicPartitionOffsets.put(cgDescription.keySet().iterator().next(), descriptions.resultAt(i + 1));
                 }
-                list.sort(new CommonHandler.ConsumerGroupComparator());
+
+                Map<TopicPartition, ListOffsetsResultInfo> topicPartitionListOffsetsResultInfo = new HashMap<>();
+                Promise<Map<TopicPartition, ListOffsetsResultInfo>> listOffsetsEndPromise = Promise.promise();
+                ac.listOffsets(topicPartitionOffsetSpecs, listOffsetsEndPromise);
+
+                return CompositeFuture.join(Future.succeededFuture(cgDescriptions),
+                                            Future.succeededFuture(cgTopicPartitionOffsets),
+                                            listOffsetsEndPromise.future());
+            })
+            .compose(composite -> {
+                Map<String, ConsumerGroupDescription> cgDescriptions = composite.resultAt(0);
+                Map<String, Map<TopicPartition, OffsetAndMetadata>> cgTopicPartitionOffsets = composite.resultAt(1);
+                Map<TopicPartition, ListOffsetsResultInfo> endOffsets = composite.resultAt(2);
+
+                List<Types.ConsumerGroupDescription> list = cgDescriptions.entrySet().stream()
+                    .map(e -> getConsumerGroupsDescription(pattern, Collections.singletonMap(e.getKey(), e.getValue()), cgTopicPartitionOffsets.get(e.getKey()), endOffsets).get(0))
+                    .filter(i -> i != null)
+                    .collect(Collectors.toList());
 
                 if (offset > list.size()) {
                     return Future.failedFuture(new InvalidRequestException("Offset (" + offset + ") cannot be greater than consumer group list size (" + list.size() + ")"));
                 }
+
+                list.sort(new CommonHandler.ConsumerGroupComparator());
+
                 int tmpLimit = limit;
                 if (tmpLimit == 0) {
                     tmpLimit = list.size();
@@ -121,7 +148,7 @@ public class ConsumerGroupOperations {
                         if (res.failed()) {
                             prom.fail(res.cause());
                         } else {
-                            Types.ConsumerGroupDescription groupDescription = getConsumerGroupsDescription(Pattern.compile(".*"), res.result().resultAt(0), res.result().resultAt(1)).get(0);
+                            Types.ConsumerGroupDescription groupDescription = getConsumerGroupsDescription(Pattern.compile(".*"), res.result().resultAt(0), res.result().resultAt(1), null).get(0);
                             if ("dead".equalsIgnoreCase(groupDescription.getState())) {
                                 prom.fail(new GroupIdNotFoundException("Group " + groupDescription.getGroupId() + " does not exist"));
                                 ac.close();
@@ -136,28 +163,43 @@ public class ConsumerGroupOperations {
     }
 
     public static void describeGroup(KafkaAdminClient ac, Promise prom, List<String> groupToDescribe) {
-        Promise describeGroupPromise = Promise.promise();
-        Promise listOffsetsPromise = Promise.promise();
+        Promise<Map<String, ConsumerGroupDescription>> describeGroupPromise = Promise.promise();
         ac.describeConsumerGroups(groupToDescribe, describeGroupPromise);
-        ac.listConsumerGroupOffsets(groupToDescribe.get(0), listOffsetsPromise);
 
-        CompositeFuture.join(describeGroupPromise.future(), listOffsetsPromise.future())
-                .onComplete(res -> {
-                    if (res.failed()) {
-                        prom.fail(res.cause());
-                    } else {
-                        Types.ConsumerGroupDescription groupDescription = getConsumerGroupsDescription(Pattern.compile(".*"), res.result().resultAt(0), res.result().resultAt(1)).get(0);
-                        if ("dead".equalsIgnoreCase(groupDescription.getState())) {
-                            prom.fail(new GroupIdNotFoundException("Group " + groupDescription.getGroupId() + " does not exist"));
-                        }
-                        prom.complete(groupDescription);
+        describeGroupPromise.future()
+            .compose(descriptions -> {
+                Promise<Map<TopicPartition, OffsetAndMetadata>> groupOffsetsPromise = Promise.promise();
+                ac.listConsumerGroupOffsets(groupToDescribe.get(0), groupOffsetsPromise);
+
+                Promise<Map<TopicPartition, ListOffsetsResultInfo>> listOffsetsEndPromise = Promise.promise();
+                List<TopicPartition> topicPartitions = getTopicPartitions(descriptions);
+                ac.listOffsets(topicPartitions.stream().collect(Collectors.toMap(k -> k, k -> OffsetSpec.LATEST)), listOffsetsEndPromise);
+
+                return CompositeFuture.join(Future.succeededFuture(descriptions),
+                                            groupOffsetsPromise.future(),
+                                            listOffsetsEndPromise.future());
+            })
+            .onComplete(res -> {
+                if (res.failed()) {
+                    prom.fail(res.cause());
+                } else {
+                    Map<String, ConsumerGroupDescription> cgDescriptions = res.result().resultAt(0);
+                    Map<TopicPartition, OffsetAndMetadata> cgOffsets = res.result().resultAt(1);
+                    Map<TopicPartition, ListOffsetsResultInfo> endOffsets = res.result().resultAt(2);
+
+                    Types.ConsumerGroupDescription groupDescription = getConsumerGroupsDescription(Pattern.compile(".*"), cgDescriptions, cgOffsets, endOffsets).get(0);
+                    if ("dead".equalsIgnoreCase(groupDescription.getState())) {
+                        prom.fail(new GroupIdNotFoundException("Group " + groupDescription.getGroupId() + " does not exist"));
                     }
-                    ac.close();
-                });
+                    prom.complete(groupDescription);
+                }
+                ac.close();
+            });
     }
 
     private static List<Types.ConsumerGroupDescription> getConsumerGroupsDescription(Pattern pattern, Map<String, io.vertx.kafka.admin.ConsumerGroupDescription> consumerGroupDescriptionMap,
-                                                                                     Map<TopicPartition, OffsetAndMetadata> topicPartitionOffsetAndMetadataMap) {
+                                                                                     Map<TopicPartition, OffsetAndMetadata> topicPartitionOffsetAndMetadataMap,
+                                                                                     Map<TopicPartition, ListOffsetsResultInfo> topicPartitionListOffsetsResultInfoMap) {
         return consumerGroupDescriptionMap.entrySet().stream().map(group -> {
             Types.ConsumerGroupDescription grp = new Types.ConsumerGroupDescription();
 
@@ -178,9 +220,10 @@ public class ConsumerGroupOperations {
                         member.setPartition(pa.getPartition());
                         member.setGroupId(group.getValue().getGroupId());
                         long currentOffset = topicPartitionOffsetAndMetadataMap.get(pa) == null ? 0 : topicPartitionOffsetAndMetadataMap.get(pa).getOffset();
-                        long lag = 0; // TODO!
+                        long endOffset = topicPartitionListOffsetsResultInfoMap.get(pa) == null ? 0 : topicPartitionListOffsetsResultInfoMap.get(pa).getOffset();
+                        long lag = endOffset - currentOffset;
                         member.setLag(lag);
-                        member.setLogEndOffset(currentOffset + lag);
+                        member.setLogEndOffset(endOffset);
                         member.setOffset(currentOffset);
                         if (pattern.matcher(pa.getTopic()).matches()) {
                             log.debug("Topic matches desired pattern");
@@ -206,5 +249,18 @@ public class ConsumerGroupOperations {
             grp.setConsumers(members);
             return grp;
         }).collect(Collectors.toList());
+    }
+
+    private static List<TopicPartition> getTopicPartitions(Map<String, io.vertx.kafka.admin.ConsumerGroupDescription> consumerGroupDescriptionMap) {
+        final BinaryOperator<Set<TopicPartition>> setAccum = (result, next) -> {
+            result.addAll(next);
+            return result;
+        };
+
+        return new ArrayList<>(consumerGroupDescriptionMap.values().stream()
+            .map(consumerGroupDescription -> consumerGroupDescription.getMembers().stream()
+                .map(mem -> mem.getAssignment().getTopicPartitions())
+                .reduce(new HashSet<TopicPartition>(), setAccum)
+        ).reduce(new HashSet<TopicPartition>(), setAccum));
     }
 }
