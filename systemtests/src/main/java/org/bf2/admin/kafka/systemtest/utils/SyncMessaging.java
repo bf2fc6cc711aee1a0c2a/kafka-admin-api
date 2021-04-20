26fe7227d1239ab8ed6c053871fda059c167b7aa
Ondrej Babec <ond.babec@gmail.com>
2021-04-20 12:41:58 +0200

    Debug run

--- systemtests/src/main/java/org/bf2/admin/kafka/systemtest/utils/SyncMessaging.java
+++ systemtests/src/main/java/org/bf2/admin/kafka/systemtest/utils/SyncMessaging.java
@@ -31,7 +31,6 @@ public class SyncMessaging {
                 KafkaConsumer<String, String> c = new KafkaConsumer<>(ClientsConfig.getConsumerConfigOauth(bootstrap, groupIds.get(i), token));
                 c.subscribe(Collections.singletonList(topicName));
                 c.poll(Duration.ofSeconds(5));
-                DynamicWait.waitForGroupExists(groupIds.get(i), kafkaClient);
                 LOGGER.info("Created " + i + ". group");
                 c.close();
             }
@@ -52,7 +51,6 @@ public class SyncMessaging {
                 KafkaConsumer<String, String> c = new KafkaConsumer<>(ClientsConfig.getConsumerConfig(bootstrap, groupIds.get(i)));
                 c.subscribe(Collections.singletonList(topicName));
                 c.poll(Duration.ofSeconds(5));
-                DynamicWait.waitForGroupExists(groupIds.get(i), kafkaClient);
                 LOGGER.info("Created " + i + ". group");
                 c.close();
             }
