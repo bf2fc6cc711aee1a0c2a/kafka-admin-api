aba342f2adf0893b4051762d6248c3a26926da68
Wojciech Trocki <wtrocki@redhat.com>
2021-05-31 14:42:17 +0100

    Update Operations.java

--- kafka-admin/src/main/java/org/bf2/admin/kafka/admin/Operations.java
+++ kafka-admin/src/main/java/org/bf2/admin/kafka/admin/Operations.java
@@ -7,7 +7,7 @@ public class Operations {
     public static final String CREATE_TOPIC = "createTopic";
     public static final String UPDATE_TOPIC = "updateTopic";
     public static final String GET_CONSUMER_GROUP = "getConsumerGroupById";
-    public static final String GET_CONSUMER_GROUPS_LIST = "getConsumerGroupList";
+    public static final String GET_CONSUMER_GROUPS_LIST = "listConsumerGroups";
     public static final String DELETE_CONSUMER_GROUP = "deleteConsumerGroupById";
     public static final String RESET_CONSUMER_GROUP_OFFSET = "resetConsumerGroupOffset";
 }
