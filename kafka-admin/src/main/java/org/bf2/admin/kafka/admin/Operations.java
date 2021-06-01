6f4d06424e6e80b322d5709625f8c1e6c1f8a8ec
Wojciech Trocki <wtrocki@redhat.com>
2021-06-01 12:51:33 +0100

    chore: change list to get

--- kafka-admin/src/main/java/org/bf2/admin/kafka/admin/Operations.java
+++ kafka-admin/src/main/java/org/bf2/admin/kafka/admin/Operations.java
@@ -2,12 +2,12 @@ package org.bf2.admin.kafka.admin;
 
 public class Operations {
     public static final String GET_TOPIC = "getTopic";
-    public static final String GET_TOPICS_LIST = "listTopics";
+    public static final String GET_TOPICS_LIST = "getTopics";
     public static final String DELETE_TOPIC = "deleteTopic";
     public static final String CREATE_TOPIC = "createTopic";
     public static final String UPDATE_TOPIC = "updateTopic";
     public static final String GET_CONSUMER_GROUP = "getConsumerGroupById";
-    public static final String GET_CONSUMER_GROUPS_LIST = "listConsumerGroups";
+    public static final String GET_CONSUMER_GROUPS_LIST = "getConsumerGroups";
     public static final String DELETE_CONSUMER_GROUP = "deleteConsumerGroupById";
     public static final String RESET_CONSUMER_GROUP_OFFSET = "resetConsumerGroupOffset";
 }
