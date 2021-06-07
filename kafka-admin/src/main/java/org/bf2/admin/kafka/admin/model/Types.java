c81b3c3c52a8591cab72e69ecca42664960ed1e6
Stanislav Knot <sknot@redhat.com>
2021-06-07 12:57:38 +0200

    Support increasing partitions
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- kafka-admin/src/main/java/org/bf2/admin/kafka/admin/model/Types.java
+++ kafka-admin/src/main/java/org/bf2/admin/kafka/admin/model/Types.java
@@ -227,6 +227,7 @@ public class Types {
     }
 
     public static class UpdatedTopic {
+        private Integer partitions;
         private String name;
         private List<NewTopicConfigEntry> config;
 
@@ -254,6 +255,14 @@ public class Types {
 
         public void setConfig(List<NewTopicConfigEntry> config) {
             this.config = config;
+        }
+
+        public Integer getPartitions() {
+            return partitions;
+        }
+
+        public void setPartitions(Integer partitions) {
+            this.partitions = partitions;
         }
     }
 
