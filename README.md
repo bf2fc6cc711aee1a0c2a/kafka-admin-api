dbbbb33497cb7b337f95c71036b187a2709fa5fb
Michael Edgar <medgar@redhat.com>
2022-07-26 15:24:38 -0400

    Add Sonar badges, remove dead code flagged as unreliable by Sonar
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![License](https://img.shields.io/badge/license-Apache--2.0-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0)
+[![License](https://img.shields.io/badge/license-Apache--2.0-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0)[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=bf2fc6cc711aee1a0c2a_kafka-admin-api&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=bf2fc6cc711aee1a0c2a_kafka-admin-api)[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=bf2fc6cc711aee1a0c2a_kafka-admin-api&metric=coverage)](https://sonarcloud.io/summary/new_code?id=bf2fc6cc711aee1a0c2a_kafka-admin-api)
 
 # Managed Kafka Admin API
 
--- systemtests/src/main/java/org/bf2/admin/kafka/systemtest/utils/TestUtils.java
+++ systemtests/src/main/java/org/bf2/admin/kafka/systemtest/utils/TestUtils.java
@@ -1,11 +1,6 @@
 package org.bf2.admin.kafka.systemtest.utils;
 
-import org.bf2.admin.kafka.systemtest.Environment;
 import org.jboss.logging.Logger;
-import org.junit.jupiter.api.extension.ExtensionContext;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class TestUtils {
     protected static final Logger LOGGER = Logger.getLogger(TestUtils.class);
@@ -20,19 +15,5 @@ public class TestUtils {
         LOGGER.info("-----------------------------------------");
         LOGGER.info(text);
         LOGGER.info("-----------------------------------------");
-    }
-
-    public static Path getLogPath(String folderName, ExtensionContext context) {
-        String testMethod = context.getDisplayName();
-        Class<?> testClass = context.getTestClass().get();
-        return getLogPath(folderName, testClass, testMethod);
-    }
-
-    public static Path getLogPath(String folderName, Class<?> testClass, String testMethod) {
-        Path path = Environment.LOG_DIR.resolve(Paths.get(folderName, testClass.getName()));
-        if (testMethod != null) {
-            path = path.resolve(testMethod.replace("(", "").replace(")", ""));
-        }
-        return path;
     }
 }
